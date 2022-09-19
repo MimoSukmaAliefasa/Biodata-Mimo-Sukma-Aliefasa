178a81023ea6353aefb4ab8bb29a89ca51bdb8bf
Mimo Sukma Aliefasa <113003619+MimoSukmaAliefasa@users.noreply.github.com>
2022-09-19 19:02:34 +0700

    Add files via upload

---
+++ TUGAS 1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama: Mimo Sukma Aliefasa" << endl;
+    cout << "NIM: A11.2022.14592" << endl;
+    cout << "Fakultas: Ilmu Komputer" << endl;
+    cout << "Prodi: Teknik Informatika" << endl;
+    cout << "Alamat: JL.kartini No:14 Batang" << endl;
+    cout << "TTL: Semarang, 12 April 2004" << endl;
+    return 0;
+}
