e47cfef573fffe7fbd19cf01af131e42ef661a6c
Mimo Sukma Aliefasa <113003619+MimoSukmaAliefasa@users.noreply.github.com>
2022-10-04 18:18:10 +0700

    Add files via upload

---
+++ TUGAS 4/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() (
+    int x;
+
+    cout<<"masukkan temperatur (c) : ";
+
+    cin>> x;
+
+    if (x <= 0)
+    {
+    cout << "ice" << endl;
+    )
+    else if (x >= 100)
+    (
+    cout<< "gas" <<endl;
+    )
+    else
+    (
+     cout<< "liquid" <<endl;
+    )
+    return 0;
+}
