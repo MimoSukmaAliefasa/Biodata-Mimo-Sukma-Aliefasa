d68860d1ee562b88c27e6451d2377a466d78e011
Mimo Sukma Aliefasa <113003619+MimoSukmaAliefasa@users.noreply.github.com>
2022-10-18 23:48:32 +0700

    Add files via upload

---
+++ Catatan/input output/main.cpp
@@ -0,0 +1,18 @@
+/*--------------
+Mencetak Nama
+--------------*/
+
+#include <iostream>
+#include <conio.h>>
+
+using namespace std;
+
+int main()
+{
+    char nama[35];
+    cout << "Nama Anda : "; cin.getline(nama, sizeof(nama));
+    cout << "Hai, "<< nama <<".Selamat Pagi Sahabat\n";
+    return 0;
+}
+
+//input output nama//
