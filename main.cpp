c2ba4bb0b71dec8fa19da2a1f2ed3e35bbd9d550
Mimo Sukma Aliefasa <113003619+MimoSukmaAliefasa@users.noreply.github.com>
2022-10-18 23:29:46 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,50 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
 
 int main()
 {
-    cout << "Nama: Mimo Sukma Aliefasa" << endl;
-    cout << "NIM: A11.2022.14592" << endl;
-    cout << "Fakultas: Ilmu Komputer" << endl;
-    cout << "Prodi: Teknik Informatika" << endl;
-    cout << "Alamat: JL.kartini No:14 Batang" << endl;
-    cout << "TTL: Semarang, 12 April 2004" << endl;
+    int i=0;
+
+    //deskripsi
+    //looping menggunakan "do while"
+    do
+    {
+        printf("%d SELAMAT PAGI SAHABAT \n",i+1);
+        i++;
+    }
+    while(i<30);
     return 0;
 }
+//kamus
+main()
+{
+    int i=0;
+
+    //deskripsi
+    //looping menggunakan "for"
+    for(i=0;i<30;i++)
+    {
+        printf("%d SELAMAT PAGI SAHABAT\n",i+1);
+    }
+    return 0;
+}
+//kamus
+main()
+{
+    int i=0;
+
+    //deskripsi
+    //looping mengguunakan "while"
+    while(i<30)
+    {
+        printf("%d SELAMAT PAGI SAHABAT\n",i+1);
+        i++
+    }
+    return 0;
+}
+
+
+
+
