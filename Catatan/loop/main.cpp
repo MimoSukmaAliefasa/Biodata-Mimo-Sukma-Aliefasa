d73501872171338f1e1d8575bc5953a91fd82099
Mimo Sukma Aliefasa <113003619+MimoSukmaAliefasa@users.noreply.github.com>
2022-10-18 23:47:36 +0700

    Add files via upload

---
+++ Catatan/loop/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int i=0;
+
+    //deskripsi
+    //looping menggunakan "do while"
+    do
+    {
+        printf("%d SELAMAT PAGI SAHABAT \n",i+1);
+        i++;
+    }
+    while(i<30);
+    return 0;
+}
+//kamus
+main()
+{
+    int i=0;
+
+    //deskripsi
+    //looping menggunakan "for"
+    for(i=0;i<30;i++)
+    {
+        printf("%d SELAMAT PAGI SAHABAT\n",i+1);
+    }
+    return 0;
+}
+//kamus
+main()
+{
+    int i=0;
+
+    //deskripsi
+    //looping mengguunakan "while"
+    while(i<30)
+    {
+        printf("%d SELAMAT PAGI SAHABAT\n",i+1);
+        i++
+    }
+    return 0;
+}
+
+
+
+
